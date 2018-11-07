6c45eb598be98fc52813328a9344cd44322bd1a7
chiubaca <alexchiu11@gmail.com>
2018-11-07 21:04:44 +0000

    number and operator mapped to buttons and displaying on the screen

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 new Vue({
     el: "#vue-calculator",
     data: {
-        output: 0
+        display: "",
+        calculation:{
+            valueA:"",
+            valueB:"",
+            operator:""
+        },
     },
     methods: {
+       
+        numberInput: function(number){
+        
+            //check if operator is empty, if so push into calculation.valueA
+            if(this.calculation.operator.length === 0 ){
+                this.display += number;
+                this.calculation.valueA += number;            
+            }else{
+                this.display += number;
+                this.calculation.valueB += number;  
+            }
+            //if operator is not an empty string, push append to valueB
+            console.log("Value A", this.calculation.valueA)
+            console.log("Value B", this.calculation.valueB)
+        },
+        setOperator:function(operator){
+            this.display += operator;
+            this.calculation.operator += operator;  
+        }
+        
     },
     computed: {
 
