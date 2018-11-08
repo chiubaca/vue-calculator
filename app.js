f88658918ee198f6fd0978f8ad00cb93413c90b3
chiubaca <alexchiu11@gmail.com>
2018-11-08 20:50:32 +0000

    plus and minus functions working

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ new Vue({
     methods: {
        
         numberInput: function(number){
-        
             //check if operator is empty, if so push into calculation.valueA
             if(this.calculation.operator.length === 0 ){
                 this.display += number;
@@ -26,8 +25,38 @@ new Vue({
         },
         setOperator:function(operator){
             this.display += operator;
-            this.calculation.operator += operator;  
+            this.calculation.operator += operator; 
+            console.log(this.calculation.operator) 
+        },
+        clearAll:function(){
+            this.display = "";
+            this.calculation.valueA = "";
+            this.calculation.valueB = ""; 
+            this.calculation.operator = "";
+            console.log("calculation object reset", this.calculation );
+            // console.log(this.calculation.valueA );
+            // console.log(this.calculation.valueB);
+            // console.log(this.calculation.operator); 
+        },
+        clearAndKeepValueA:function(){
+            this.calculation.valueB = ""; 
+            this.calculation.operator = "";
+        },
+        equate:function(){
+            // console.log("hello from equate")
+            // console.log(this.operator)
+            switch(this.calculation.operator){
+                case "+":
+                    this.display =  Number(this.calculation.valueA) + Number(this.calculation.valueB);
+                    this.calculation.valueA = this.display;
+                    this.clearAndKeepValueA();
+                case "-":
+                    this.display =  Number(this.calculation.valueA) - Number(this.calculation.valueB);
+                    this.calculation.valueA = this.display;
+                    this.clearAndKeepValueA();
+            }
         }
+
         
     },
     computed: {
